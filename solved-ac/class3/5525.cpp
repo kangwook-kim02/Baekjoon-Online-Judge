facb9790c891928450a1daf0c7db85a0c0223350
Kangwook Kim <ices17@naver.com>
2026-01-18 15:39:00 +0900

    Add files via upload

--- solved-ac/class3/5525.cpp
+++ solved-ac/class3/5525.cpp
@@ -1,10 +1,11 @@
 /*
-BOJ No.5525
-IOIOI
-Sliding Window
+BOJ No.5430
+AC
 */
 #include <iostream>
 #include <vector>
+#include <string>
+#include <stack>
 using namespace std;
 
 int main() {
@@ -12,29 +13,86 @@ int main() {
 	cout.tie(0);
 	ios::sync_with_stdio(0);
 
-	int N, M;
-	cin >> N >> M;
-	string S;
-	cin >> S;
 
-	int count = 0;
-	
-	int k = 0; // IOIÀÇ °³¼ö
-	for (int i = 1; i + 1 < M;) {
-		if (S[i - 1] == 'I' && S[i] == 'O' && S[i + 1] == 'I') {
-			k++;
-			if (k == N) {
-				count++;
-				k--;
+	int T;
+	cin >> T;
+
+	while (T--) {
+		bool error = false;
+		string Func;
+		cin >> Func;
+		
+		int arrSize;
+		cin >> arrSize;
+		
+		vector<string> v;
+
+		string arr;
+		cin >> arr;
+
+		string number = "";
+		for (int i = 1; i < arr.size(); i++) {
+			if (arr[i] == ',' || arr[i] == ']') {
+				if (number != "") {
+					v.push_back(number);
+					number = "";
+					continue;
+				}
 			}
-			i = i + 2;
+			number += arr[i];
 		}
-		else {
-			k = 0;
-			i++;
-		}
-	}
 
-	cout << count;
-	
+		int left = 0;
+		int right = arrSize - 1;
+		bool reverse = false;
+
+		for (int i = 0; i < Func.size(); i++) {
+			if (Func[i] == 'R') { // Reverse
+				reverse = reverse == true ? false : true;
+			}
+			else { // pop
+
+				if (arrSize == 0) {
+					error = true;
+					continue;
+				}
+
+
+				if (reverse) {
+					right--;
+					arrSize--;
+				}
+				else {
+					left++;
+					arrSize--;
+				}
+			}
+		}
+
+		if (error) {
+			cout << "error" << "\n";
+			continue;
+		}
+
+		stack<string> stk;
+		if (reverse) {
+			for (int i = left; i <= right; i++) {
+				stk.push(v[i]);
+			}
+
+			for (int i = left; i <= right; i++) {
+				v[i] = stk.top();
+				stk.pop();
+			}
+		}
+
+		cout << "[";
+		for (int i = left; i <= right; i++) {
+			cout << v[i];
+			if (i < right) {
+				cout << ",";
+			}
+		}
+		cout << "]" << "\n";
+	}
 }
