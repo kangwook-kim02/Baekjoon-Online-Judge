e52b74293c8877353f8ff2b2e53f80bce5a95a1e
Kangwook Kim <ices17@naver.com>
2026-07-15 14:37:46 +0900

    Add solution function for unique elements count
    Implement solution to find maximum unique elements from a list.

---
+++ programmers/2.cpp
@@ -0,0 +1,25 @@
+/*
+프로그래머스
+해시
+Level 1
+*/
+
+#include <vector>
+#include <set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    set<int> s;
+    for (int i = 0; i < nums.size(); i++) {
+        s.insert(nums[i]);
+    }
+    
+    if (s.size() >= nums.size() / 2) 
+        answer = nums.size() / 2;
+    else
+        answer = s.size();
+    
+    return answer;
+}
